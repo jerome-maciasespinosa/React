5e76b20ccf68efa5f2f6bbc80326e61ce7dfd734
jérôme macias espinosa <jerome2103@live.fr>
2018-04-24 13:15:57 +0200

    redux - assignments - 24-04-2018

--- redux-start/src/containers/Counter/Counter.js
+++ redux-start/src/containers/Counter/Counter.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
@@ -28,14 +29,29 @@ class Counter extends Component {
     render () {
         return (
             <div>
-                <CounterOutput value={this.state.counter} />
-                <CounterControl label="Increment" clicked={() => this.counterChangedHandler( 'inc' )} />
-                <CounterControl label="Decrement" clicked={() => this.counterChangedHandler( 'dec' )}  />
-                <CounterControl label="Add 5" clicked={() => this.counterChangedHandler( 'add', 5 )}  />
-                <CounterControl label="Subtract 5" clicked={() => this.counterChangedHandler( 'sub', 5 )}  />
+                <CounterOutput value={this.props.ctr} />
+                <CounterControl label="Increment" clicked={() => this.props.onCounterChange('INCREMENT')} />
+                <CounterControl label="Decrement" clicked={() => this.props.onCounterChange('DECREMENT')}  />
+                <CounterControl label="Add 5" clicked={() => this.props.onCounterChange( 'ADD', 5 )}  />
+                <CounterControl label="Subtract 5" clicked={() => this.props.onCounterChange( 'SUB', 5 )}  />
             </div>
         );
     }
 }
 
-export default Counter;
+const mapStateToProps = state => {
+    return {
+        ctr: state.counter
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+   
+    return {
+        onCounterChange: (operation) => dispatch({type: operation}),
+        onCounterChange: (operation, value) => {dispatch({type: operation, value: value})}
+
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
